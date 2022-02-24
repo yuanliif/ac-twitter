a6960e161bb58779cc9132884df98224b10e5ff9
erase2004 <erase2004@gmail.com>
2022-02-24 23:34:40 +0800

    1. 設定主容器CSS style，讓頁面保持置中

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,13 @@ export default {}
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
 
+#app {
+  height: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 1440px;
+}
+
 // 通用按鈕顏色樣式
 .btn-control {
   background-color: #FF6600;
